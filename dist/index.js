e4c991595eb503da410efa3f25f8c6eb313fa4e1
UnrelatedString <unrelatedstring@gmail.com>
2025-05-18 19:34:44 -0400

    basic working first commit before I have to dive into CSS Hell and/or use foreigns for offscreen canvas stuff lmao

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+(function(n){return n.pure})({pure:function(n){return function(){return n}}})(undefined)();
---
+++ rollup.config.ts
@@ -0,0 +1,41 @@
+import type { RollupOptions } from 'rollup';
+import dev from 'rollup-plugin-dev';
+import html from '@rollup/plugin-html';
+import terser from '@rollup/plugin-terser';
+import copy from 'rollup-plugin-copy';
+
+const config = (args: Record<string, any>): RollupOptions => ({
+	input: 'src/index.mjs',
+	output: {
+		dir: 'dist',
+		format: 'es'
+	},
+    plugins: [
+        copy({
+            targets: [
+                { src: 'static/**/*', dest: 'dist' }
+            ],
+        }),
+        html({
+            title: "bingous",
+            meta: [
+                { charset: 'utf-8' },
+                { property: 'og:type', content: 'website' },
+                { property: 'og:title', content: '44ko' },
+                { property: 'og:description', content: 'A content-aware manga reader' },
+                //{ property: 'og:url', content: 'https://www.bingous.org' },
+                //{ property: 'og:image', content: 'https://www.bingous.org/bingous1.png' },
+            ],
+            attributes: {
+                html: { lang: 'en' },
+                link: { rel: 'icon', href: 'favicon.ico' },
+            },
+        }),
+        dev({
+            dirs: ["dist"],
+            force: args.configServe,
+        }),
+        terser(),
+    ],
+});
+export default config;
