c1f4a4e473d10c4bef3f664728b24e2d3331f2a5
UnrelatedString <unrelatedstring@gmail.com>
2025-05-19 11:50:36 -0400

    no warnings! :D and aslso in a new branch so i stop doing all these deploys of literally no visible changes lmao

--- src/Yoyocomponent/Root.purs
+++ src/Yoyocomponent/Root.purs
@@ -8,14 +8,18 @@ import Halogen.HTML as HTML
 root
   :: forall query input output m.
   H.Component query input output m
-
 root = H.mkComponent
   { initialState
   , render
   , eval: H.mkEval H.defaultEval
   }
+  where
+
+  render
+    :: forall state action
+    . state
+    -> H.ComponentHTML action () m
+  render _ = HTML.div_ []
 
 initialState :: forall a. a -> Unit
 initialState = const unit
-
-render _ = HTML.div_ []
