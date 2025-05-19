37211aa7a236fb82a707ea748615bbc3fa4313e7
UnrelatedString <unrelatedstring@gmail.com>
2025-05-19 15:26:47 -0400

    ...how am I already overengineering something else...

--- src/Foreign/Jank.purs
+++ src/Foreign/Jank.purs
@@ -1,5 +1,8 @@
 module Foreign.Jank
   ( OffscreenCanvas
+  , OffscreenCanvasContextType
+  , offscreen2d
+  , offscreenBitmap
   , createOffscreenCanvas
   , drawImage
   , ImageBitmap
@@ -16,14 +19,26 @@ import Web.HTML (HTMLImageElement)
 
 foreign import data OffscreenCanvas :: Type
 
+newtype OffscreenCanvasContextType = OffscreenCanvasContextType String
+
+offscreen2d :: OffscreenCanvasContextType
+offscreen2d = OffscreenCanvasContextType "2d"
+
+offscreenBitmap :: OffscreenCanvasContextType
+offscreenBitmap = OffscreenCanvasContextType "bitmaprenderer"
+
 foreign import data ImageBitmap :: Type
 
 foreign import createOffscreenCanvasImpl
-  :: E.EffectFn1
+  :: E.EffectFn2
     { width :: Int, height :: Int }
+    OffscreenCanvasContextType
     OffscreenCanvas
 
-createOffscreenCanvas :: { width :: Int, height :: Int } -> Effect OffscreenCanvas
+createOffscreenCanvas
+  :: { width :: Int, height :: Int }
+  -> OffscreenCanvasContextType
+  -> Effect OffscreenCanvas
 createOffscreenCanvas = E.runEffectFn1 createOffscreenCanvasImpl
 
 foreign import drawImageImpl
@@ -40,6 +55,11 @@ foreign import createImageBitmapImpl
     OffscreenCanvas
     { x :: Int, y :: Int, width :: Int, height :: Int }
     (Promise ImageBitmap)
+
+-- COULD do this with CSS
+-- https://stackoverflow.com/a/26219379
+-- but seems cleaner not to juggle URLs when it's
+-- canvas stuff anyways
 
 crop
   :: OffscreenCanvas
