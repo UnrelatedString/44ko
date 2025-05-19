3144d3b217df5266bb4b4c1d7b3dc43170776e58
UnrelatedString <unrelatedstring@gmail.com>
2025-05-18 21:25:18 -0400

    oh yeah no I will need foreign offscreen stuff for content parsing anyways lol. and may as well make that its own component even if it literally doesn't exist in the UI because then like it can be by page and whatever

--- src/Main.purs
+++ src/Main.purs
@@ -3,6 +3,13 @@ module Main where
 import Prelude
 
 import Effect (Effect)
+import Halogen.Aff (runHalogenAff, awaitBody)
+import Halogen.VDom.Driver (runUI)
+
+import Component.Root (root)
 
 main :: Effect Unit
-main = pure unit
+main = runHalogenAff do
+  body <- awaitBody
+  runUI root unit body
+
