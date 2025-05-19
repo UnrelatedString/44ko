d0184577bb9c564f10e96a117bcd4e918ef99430
UnrelatedString <unrelatedstring@gmail.com>
2025-05-18 23:11:23 -0400

    wait there are existing blob bindings BUT they're kinda shit because object URLs are literally just raw strings like what why ????? oh wait it literally is just a string of course wow JS sure is a language I guess the. it probably performs fine because it does some object identity stuff anyways instead of actually hashing an entire giant blob url to compare it? idk ;_;

---
+++ src/Foreign/Jank.purs
@@ -0,0 +1,11 @@
+module Foreign.Jank
+  ( OffscreenCanvas
+  , ImageBitmap
+  , ObjectUrl
+  ) where
+
+import Prelude
+
+foreign import data OffscreenCanvas :: Type
+
+foreign import data ImageBitmap :: Type
