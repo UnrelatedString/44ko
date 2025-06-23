884e8b3c8b5dd7ca768284e55f7586b143372c75
UnrelatedString <unrelatedstring@gmail.com>
2025-06-23 17:21:06 -0400

    I am downright SHOCKED that I wrote this better stub placeholder right on the first try with zero compile errors LMAOOOO

--- src/Yoyocomponent/Root.purs
+++ src/Yoyocomponent/Root.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Halogen as H
 import Halogen.HTML as HTML
+import Halogen.HTML.Properties as Prop
 
 root
   :: forall query input output m.
@@ -15,7 +16,21 @@ root = H.mkComponent
   , eval: H.mkEval H.defaultEval
   }
 
+type State = Unit
+
 initialState :: forall a. a -> Unit
 initialState = const unit
 
-render _ = HTML.div_ []
+render :: forall m action. State -> H.ComponentHTML action () m
+render _ = HTML.div_
+  [ HTML.h1_ [HTML.text "mentos cola!"]
+  , HTML.p_
+    [ HTML.text "this website is free and open source software under the "
+    , HTML.b_ [HTML.text "gnu affero general public license version 3.0 or later"]
+    , HTML.text ". source code is available at "
+    , HTML.a [Prop.href "https://github.com/UnrelatedString/44ko"]
+      [ HTML.text "github.com/UnrelatedString/44ko"
+      ]
+    , HTML.text ", where bug reports and suggestions are also always welcome."
+    ]
+  ]
